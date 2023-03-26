36ed43abc9bac3eeabe9307ac63974842c4be0c8
Willian Marioti <willian.marioti@gmail.com>
2023-03-26 14:34:36 -0300

    Primeiras alterações.

---
+++ ToDoList/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    internal class Person
+    {
+        private string IdPerson;
+        public string Name { get; set; }
+
+
+        Person(string m)
+        {
+            var temp = Guid.NewGuid();
+            IdPerson = temp.ToString();
+            SetName(m);
+        }
+
+        Person SetName(string name)
+        {
+            Console.WriteLine("Informe seu nome: ");
+            name = Console.ReadLine();
+            return new Person(name);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ ToDoList/Todo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace ToDoList
+{
+    internal class Todo
+    {
+        private string id;
+
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Status { get; set; }
+
+        public Todo(string description)
+        {
+            var temp = Guid.NewGuid();
+            id = temp.ToString();
+
+            this.Description = description;
+
+            var date = new DateTime();
+            date = DateTime.Now.ToUniversalTime();
+            Console.WriteLine(date);
+
+            Console.WriteLine("");
+
+            //incrementar outras variáveis
+
+
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + id + "Description: " + Description + "Category: "+ "Date Created: " + Created + "DateTime: " + DateTime.ToString()
+                + "Status: " + Status;
+        }
+
+        string ToFile()
+        { //ver sobre o retorno da categoria
+            return id + ";" + Description + ";" + Created + ";" + DateTime.ToString() + Status;
+        }
+
+        bool SetStatus()
+        {
+
+        }
+
+        Person SetPerson()
+        {
+
+        }
+    }
+}
