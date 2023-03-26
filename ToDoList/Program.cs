e79b9591d0d78e6c5ee9a47c4bd2197e2e67528f
Ana Cecilia Grossi Bolito <anac.grossi@icloud.com>
2023-03-26 16:46:55 -0300

    incremento na estrutura básica do toDoList

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,5 +1,32 @@
 ﻿internal class Program
 {
+    static int Menu()
+        {
+            int menu;
+
+            Console.Clear();
+            Console.WriteLine(">>>> Menu Principal <<<<");
+            Console.WriteLine("1- Adicionar tarefa ");
+            Console.WriteLine("2- tot Tarefas Importantes");
+            Console.WriteLine("3- tot Tarefas de Trabalho");
+            Console.WriteLine("4- tot Tarefas de Estudo");
+            Console.WriteLine("5- tot Tarefas concluídas");
+            Console.WriteLine("6- tot Tarefas a concluir");
+
+            Console.WriteLine("?- Sair da execução");
+
+            try
+            {
+                menu = int.Parse(Console.ReadLine());
+            }
+
+            catch
+            {
+                return '\n';
+            }
+
+            return menu;
+        }
     private static void Main(string[] args)
     {
         
